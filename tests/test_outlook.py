c461d990ebd79d9989fad269ad1419597b32d921
Sukhbinder Singh <sukh2010@yahoo.com>
2024-08-20 13:35:15 +0530

    Update test_outlook.py

--- tests/test_outlook.py
+++ tests/test_outlook.py
@@ -1,14 +1,34 @@
 import sys
 from unittest import mock
-from send_outlookemail import main
+from send_outlookemail import create_parser
 
 
-def test_main(tmp_path):
-    with mock.patch("win32com.client.Dispatch") as dispatch_mock:
-        dispatch_mock.return_value.CreateItem.return_value = "MockMail"
+# def test_main(tmp_path):
+#     with mock.patch("win32com.client.Dispatch") as dispatch_mock:
+#         dispatch_mock.return_value.CreateItem.return_value = "MockMail"
 
-        with mock.patch("os.path.join", return_value=str(tmp_path / "email.msg")):
-            args = [
+#         with mock.patch("os.path.join", return_value=str(tmp_path / "email.msg")):
+#             args = [
+#                 "sendmail",
+#                 "-t",
+#                 "test@example.com",
+#                 "-s",
+#                 "Test Subject",
+#                 "-b",
+#                 "Test body 1, Test body 2",
+#                 "-hb",
+#                 "<html><body>HTML Test</body></html>",
+#                 "-ds"
+#             ]
+#             sys.argv = args
+    
+#             main()
+
+#         assert dispatch_mock.called
+
+def test_create_parser():
+    paser = create_parser()
+                args = [
                 "sendmail",
                 "-t",
                 "test@example.com",
@@ -20,8 +40,7 @@ def test_main(tmp_path):
                 "<html><body>HTML Test</body></html>",
                 "-ds"
             ]
-            sys.argv = args
-    
-            main()
-
-        assert dispatch_mock.called
+    args = parser.parse_args(args)
+    assert args.to == "test@example.com"
+    assert args.dont_send
+    assert args.subject == "Test Subject"
